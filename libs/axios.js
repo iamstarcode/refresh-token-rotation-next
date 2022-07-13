3d5c2a327735df984a52b9466c5244338db29e7b
Bakare Abiola <murphybiola007@gmail.com>
2022-07-13 05:16:02 +0100

    *

--- libs/axios.js
+++ libs/axios.js
@@ -1,5 +1,7 @@
 import Axios from 'axios'
-const BASE_URL = 'http://localhost:8000'
+//const BASE_URL = 'http://localhost:8000'
+const BASE_URL  =  'https://starcode-nest-starter.herokuapp.com'
+
 
 export default Axios.create({
   baseURL: BASE_URL,
